6706ad12687ce29a3736ba79b0bbe9f944ee17bc
unknown <qldtn777@gmail.com>
2023-11-18 00:13:12 +0900

    일단 기물,아이템 시너지 툴팁 완성

--- Assets/3. OCM/Scripts/PieceToolTipPanel.cs
+++ Assets/3. OCM/Scripts/PieceToolTipPanel.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,15 +12,44 @@ public class PieceToolTip : MonoBehaviour
     public TMP_Text atkdmgTxt;
     public TMP_Text atkspdTxt;
     public Image img;
-
-    private void Start()
-    {
-        gameObject.SetActive(false);
-    }
-
-    public void SetupPieceToolTip(Sprite sprite, string name, int hp, int atkdmg, float atkspd)
+    public Image itemImg1;
+    public Image itemImg2;
+    public Image itemImg3;
+    public void SetupPieceToolTip(Sprite sprite,Sprite itemSprite1, Sprite itemSprite2, Sprite itemSprite3, string name, int hp, int atkdmg, float atkspd)
     {
         img.sprite = sprite;
+        if (itemSprite1 != null)
+        {
+            itemImg1.gameObject.GetComponent<CanvasGroup>().alpha = 1;
+            itemImg1.sprite = itemSprite1;
+        }
+        else
+        {
+            itemSprite1 = sprite;
+            itemImg1.gameObject.GetComponent<CanvasGroup>().alpha = 0 ;
+        }
+        if (itemSprite2 != null)
+        {
+            itemImg2.gameObject.GetComponent<CanvasGroup>().alpha = 1;
+            itemImg2.sprite = itemSprite2;
+        }
+        else
+        {
+            itemSprite2 = sprite;
+            itemImg2.gameObject.GetComponent<CanvasGroup>().alpha = 0;
+        }
+        if (itemSprite3 != null)
+        {
+            itemImg3.gameObject.GetComponent<CanvasGroup>().alpha = 1;
+            itemImg3.sprite = itemSprite3;
+        }
+        else
+        {
+            itemSprite3 = sprite;
+            itemImg3.gameObject.GetComponent<CanvasGroup>().alpha = 0;
+        }
+        
+        
         nameTxt.text = "name: " + name;
         hpTxt.text = "hp: " + hp.ToString();
         atkdmgTxt.text = "atkdmg: " + atkdmg.ToString();
