2f554b03cf406df33090326d87ea539a553fee84
unknown <qldtn777@gmail.com>
2023-11-18 21:23:17 +0900

    SynergySO일부수정

--- Assets/3. OCM/Scripts/SynergySO.cs
+++ Assets/3. OCM/Scripts/SynergySO.cs
@@ -5,18 +5,22 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "SE_", menuName = "SynergyData/SynergyEnhance")]
 public class SynergySO : ScriptableObject
 {
+    public Sprite icon;
+
     public string spieces;
 
     public Piece injectData;
 
     public int[] terms;
+
+    public string injectDataStr;
     public void Execute(List<Pieces> pieces, int term)
     {
-        foreach (Pieces piece in pieces) 
+        foreach (Pieces piece in pieces)
         {
-            if(piece.spieces == spieces)
+            if (piece.spieces == spieces)
             {
-                piece.buffData[2].maxHp = injectData.maxHp  * (term + 1);
+                piece.buffData[2].maxHp = injectData.maxHp * (term + 1);
                 piece.hp = piece.maxHp;
                 piece.buffData[2].attackDamage = injectData.attackDamage * (term + 1);
             }
