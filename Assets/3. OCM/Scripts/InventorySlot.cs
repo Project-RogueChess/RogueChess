1e33b023d5fd24cd4155598a647f92cd55357026
unknown <qldtn777@gmail.com>
2023-11-18 21:39:49 +0900

    아이템 툴팁 적용,맵 옮김

--- Assets/3. OCM/Scripts/InventorySlot.cs
+++ Assets/3. OCM/Scripts/InventorySlot.cs
@@ -4,48 +4,62 @@ using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using static UnityEditor.Progress;
 
-public class InventorySlot : MonoBehaviour , IPointerEnterHandler,IDropHandler, IPointerExitHandler
+public class InventorySlot : MonoBehaviour, IPointerEnterHandler, IDropHandler, IPointerExitHandler
 {
     private Image image;
     private RectTransform rect;
     public bool isSlotEmpty = true;
     public GameObject itemGO;
-    
+    public ToolTip tooltip;
+    public ItemObject itemObject;
+
     public void Awake()
     {
-       image = GetComponent<Image>();
+        image = GetComponent<Image>();
         rect = GetComponent<RectTransform>();
+        itemObject = transform.GetChild(0).GetComponent<ItemObject>();
     }
-
 
 
     public void OnPointerEnter(PointerEventData eventData)
     {
         image.color = Color.yellow;
+        if (itemObject._item.itemName != string.Empty && itemObject._item.itemName != null)
+        {
+            tooltip.gameObject.transform.position = new Vector3(1710, 810, 0);
+            tooltip.SetupItemToolTip(itemObject._item.itemSprite, itemObject._item.itemName, itemObject._item.itemHp, itemObject._item.itemAttackDamage, itemObject._item.itemAttackSpeed);
+        }
+        else
+        {
+            tooltip.gameObject.transform.position = new Vector3(2300, 400, 0);
+        }
+
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         image.color = Color.white;
+        tooltip.gameObject.transform.position = new Vector3(2300, 400, 0);
     }
 
     public void OnDrop(PointerEventData eventData)
     {
-        if ( eventData.pointerDrag != null && isSlotEmpty == true)
+        if (eventData.pointerDrag != null && isSlotEmpty == true)
         {
             if (eventData.pointerDrag.GetComponent<ItemObject>()._item == itemGO.GetComponent<ItemObject>()._item)
             {
                 itemGO.GetComponent<ItemObject>()._item = eventData.pointerDrag.GetComponent<ItemObject>()._item;
             }
-            else 
+            else
             {
                 itemGO.GetComponent<ItemObject>()._item = eventData.pointerDrag.GetComponent<ItemObject>()._item;
                 eventData.pointerDrag.GetComponent<ItemObject>()._item = new Item();
             }
             isSlotEmpty = false;
         }
-        else if(eventData.pointerDrag != null && isSlotEmpty == false)
+        else if (eventData.pointerDrag != null && isSlotEmpty == false)
         {
             //GameObject temp;
             //temp = itemGO;
@@ -55,7 +69,7 @@ public class InventorySlot : MonoBehaviour , IPointerEnterHandler,IDropHandler, 
             isSlotEmpty = false;
         }
     }
-    
+
     public void OnItemDraggedOut()
     {
         isSlotEmpty = true;
