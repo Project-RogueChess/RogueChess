3ab66293e875d6085fddf2ae7d3928541230e23e
minkee009 <minkee5004@gmail.com>
2023-11-18 18:57:00 +0900

    체력 잃을 때 화면효과

---
+++ Assets/2. JMK/Scripts/PlayerDamagedImage.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Unity.Mathematics;
+using UnityEditor.Rendering;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerDamagedImage : MonoBehaviour
+{
+    public float recoverSpeed = 2f;
+    public Image damagedImage;
+    private float _hurtGage = 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        _hurtGage = Mathf.Max(_hurtGage - Time.deltaTime * recoverSpeed, 0.0f);
+        damagedImage.color = new Color(1f,1f,1f,_hurtGage / 4);
+    }
+
+    public void SetHurtGage(int hurtAmount)
+    {
+        _hurtGage = Mathf.Clamp(math.abs(hurtAmount), 0, 4);
+    }
+}
